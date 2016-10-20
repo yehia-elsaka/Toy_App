0218e808471f0bf020ee22c5fbccc2731b2442af
yehia-elsaka <yahya.elsaka@gmail.com>
2016-10-20 17:46:51 +0300

    handl ex

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  rescue_from Exception, :with => :handle_exception
+
+  def handle_exception(exception)
+    render :json => {"error" => exception.message}
+  end
+
 
   def hello
     render html: "Hello, World!"
